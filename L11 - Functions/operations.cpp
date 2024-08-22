ca40dbb192dd04743b0dfce1793c288fbadd21e4
Swastik Purathepparambil <swastiksp25@gmail.com>
2024-08-22 12:22:22 -0700

    Functions

---
+++ L11 - Functions/operations.cpp
@@ -0,0 +1,7 @@
+// File Management kind of thing
+// This way, we can store definitions in other files
+// and just reference them when we need them for the main file
+
+int incr_mult(int a, int b){
+    return ((++a) * (++b));
+}
